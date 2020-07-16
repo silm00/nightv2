6ca8560df36a0c5e7df5e1ea72ba0ef24d5d5ab7
silmi00 <silmi00@users.noreply.github.com>
2020-07-16 21:01:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const activities_list = [
     "Jaga Kesehatan", 
     "command nc!help",
     //"PODCAST KAMIS 21.00 WIB",
-    "PODCAST RABU 20.00 WIB"
+    "Livestream Gaming JUMAT 20.00 WIB"
     ]; // creates an arraylist containing phrases you want your bot to switch through.
 
 client.on('ready', () => {
