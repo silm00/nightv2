fd0147d6806c0862bb4b1599bfcb3c555b41c245
silm00 <68076812+silm00@users.noreply.github.com>
2020-08-01 19:38:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -337,20 +337,33 @@ client.on('message', message => {
 });
 
 client.on('message', message => {
-if (message.content.startsWith(prefix + "kick")) {
-   
-   // check if user has the role
-   if (message.member.roles.some(role => role.name === 'ADMIN')) {
-    if (message.members.mentions.first()) {
-        try {
-            message.members.mentions.first().kick();
-        }
-      } 
-    }else {
-       message.channel.send("You do not have sufficient permissions to run this command!");
-  }
+if(message.content === "nc!kick") {
+    // This command must be limited to mods and admins. In this example we just hardcode the role names.
+    // Please read on Array.some() to understand this bit: 
+    // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
+    if(!message.member.roles.cache.some(roles=>["Admin", "Owner"].includes(roles.name)))
+      return message.reply("Sorry, you don't have permissions to use this!");
+    
+    // Let's first check if we have a member and if we can kick them!
+    // message.mentions.members is a collection of people that have been mentioned, as GuildMembers.
+    // We can also support getting the member by ID, which would be args[0]
+    let member = message.mentions.members.first() || message.guild.members.get(args[0]);
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.kickable) 
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+    
+    // slice(1) removes the first part, which here should be the user mention or ID
+    // join(' ') takes all the various parts to make it a single string.
+    let reason = args.slice(1).join(' ');
+    if(!reason) reason = "No reason provided";
+    
+    // Now, time for a swift kick in the nuts!
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
 
-}
+  }
 });
 
 
