2dbbf5fb7e45422d1f32c72a158d34dd86d3a92e
silm00 <68076812+silm00@users.noreply.github.com>
2020-08-01 19:01:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -336,44 +336,7 @@ client.on('message', message => {
   }
 });
 
-client.on('message', message => {
-  if (msg.member.hasPermission("BAN_MEMBERS")) {
-if (message.content.startsWith("$kick")) {
 
-    if (!message.member.roles.find("name", "Admin"))
-        return;
-
-    // Easy way to get member object though mentions.
-    var member = message.mentions.members.first();
-    // Kick
-    member.kick().then((member) => {
-        // Successmessage
-        message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
-    }).catch(() => {
-        // Failmessage
-        message.channel.send("Access Denied");
-    });
-}
-}
-});
-client.on("message", (message) => {
-    if (message.content.startsWith("$ban")) {
-
-        if (!message.member.roles.find("name", "Admin"))
-            return;
-
-        // Easy way to get member object though mentions.
-        var member = message.mentions.members.first();
-        // ban
-        member.ban().then((member) => {
-            // Successmessage
-            message.channel.send(":wave: " + member.displayName + " has been successfully banned https://gfycat.com/playfulfittingcaribou :point_right: ");
-        }).catch(() => {
-            // Failmessage
-            message.channel.send("Access Denied");
-        });
-    }
-});
 
 
 //NzMwMjcxODg0MTE3MTQ3NjQ4.XwVE0A.WqM0Owv_y-GqWCp06slkdIUxH0Q
