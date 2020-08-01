36bb02f35d95c3031f98fb63152a777b7e67fd46
silm00 <68076812+silm00@users.noreply.github.com>
2020-08-01 20:07:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -336,7 +336,51 @@ client.on('message', message => {
   }
 });
 
+client.on('message', message => {
+  // Ignore messages that aren't from a guild
+  if (!message.guild) return;
 
+  // If the message content starts with "!kick"
+  if (message.content.startsWith('nc!kick')) {
+    if(!member.roles.cache.some(role => role.name === 'Admin','Owner'))
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
+    const user = message.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user) ;
+      // If the member is in the guild
+      if (member) {
+        /*
+         * Kick the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         */
+        member
+          .kick('Optional reason that will display in the audit logs')
+          .then(() => {
+            // We let the message author know we were able to kick the person
+            message.reply(`Successfully kicked ${user.tag}`);
+          })
+          .catch(err => {
+            // An error happened
+            // This is generally due to the bot not being able to kick the member,
+            // either due to missing permissions or role hierarchy
+            message.reply('I was unable to kick the member');
+            // Log the error
+            console.error(err);
+          });
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply("That user isn't in this guild!");
+      }
+      // Otherwise, if no user was mentioned
+    } else {
+      message.reply("You didn't mention the user to kick!");
+    }
+  }
+});
 
 //NzMwMjcxODg0MTE3MTQ3NjQ4.XwVE0A.WqM0Owv_y-GqWCp06slkdIUxH0Q
 
