86a66c26eb53faf1d9c5ceab32120a0f5ead4e10
silm00 <68076812+silm00@users.noreply.github.com>
2020-08-01 20:11:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -342,7 +342,7 @@ client.on('message', message => {
 
   // If the message content starts with "!kick"
   if (message.content.startsWith('nc!kick')) {
-    if(!member.roles.cache.some(role => role.name === 'Admin','Owner')){
+    if(member.roles.cache.some(role => role.name === 'Admin','Owner')){
     // Assuming we mention someone in the message, this will return the user
     // Read more about mentions over at https://discord.js.org/#/docs/main/master/class/MessageMentions
     const user = message.mentions.users.first();
