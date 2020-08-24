696f2b7af5c44afb51571f7347e67c492b3082fa
silm00 <68076812+silm00@users.noreply.github.com>
2020-08-24 16:06:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const activities_list = [
 client.on('ready', () => {
     setInterval(() => {
         const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-        client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+        client.user.setActivity(activities_list[index] , {type : 'WATCHING'}); // sets bot's activities to one of the phrases in the arraylist.
     }, 7000); // Runs this every 10 seconds.
 });
 
